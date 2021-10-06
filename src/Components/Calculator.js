c0552bb0b5a14d7fc4a459fad5a74ead57fd9e2e
TG2408 <tanmaygupta2408@gmail.com>
2021-10-06 13:54:50 +0530

    some updates

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -6,23 +6,55 @@ class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            temperature: '',
-            scale: ''
+            temperature: '0',
+            scale: 'c'
         }
     }
 
-    handleChange = (e)=> {
-        this.setState({temperature: e.target.value});
-      }
+    // toCelsius = (fahrenheit) => {
+    //    return (fahrenheit - 32) * 5 / 9;
+    // }
+      
+    // toFahrenheit = (celsius) => {
+    //     return (celsius * 9 / 5) + 32;
+    // }
+
+    // tryConvert = (temperature, convert) => {
+    //     const input = parseFloat(temperature);
+    //     if (Number.isNaN(input)) {
+    //       return '';
+    //     }
+    //     const output = convert(input);
+    //     const rounded = Math.round(output * 1000) / 1000;
+    //     return rounded;
+    // }
+
+    handleCelciusChange = (e)=> {
+        this.setState({
+            temperature: e.target.value,
+            scale: 'c'
+        });
+    }
+
+    handleFerhaniteChange = (e) => {
+        this.setState({
+            temperature: e.target.value,
+            scale: 'f'
+        })
+    }
     
     render() {
+        
+        const celcius = this.state.scale == 'c' ? this.state.temperature : (Math.round(((this.state.temperature - 32) * 5 / 9) * 1000) / 1000).toString(); 
+        const ferhanite = this.state.scale == 'f' ? this.state.temperature : (Math.round(((this.state.temperature * 9 / 5) + 32) * 1000) / 1000 ).toString();
+
         return(
             <fieldset>
                 <legend>Enter the temperture</legend>
-                <TemperatureInput scale="c" onTemperatureChange={this.handleChange} temperature={this.state.temperature}/>
-                <TemperatureInput scale="f" onTemperatureChange={this.handleChange} temperature={this.state.temperature}/>
+                <TemperatureInput scale="c" onTemperatureChange={this.handleCelciusChange} temperature={celcius}/>
+                <TemperatureInput scale="f" onTemperatureChange={this.handleFerhaniteChange} temperature={ferhanite}/>
                 <BoilingVerdict
-                    celcius={parseFloat(this.state.temperature)}/>
+                    celcius={parseFloat(celcius)}/>
             </fieldset>
         )
     }
